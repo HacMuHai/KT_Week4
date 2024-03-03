94bf7d70124f40988c76322f93add144f5fcb96a
H31M44\Student <Student>
2024-03-03 13:01:02 +0700

    create project

---
+++ src/main/java/vn/edu/iuh/fit/Main.java
@@ -0,0 +1,16 @@
+package vn.edu.iuh.fit;
+
+import jdepend.xmlui.JDepend;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+            JDepend depend =new JDepend(new PrintWriter("reports/report.xml"));
+            depend.addDirectory("T:\\LeHuuHiep\\KT_Week4");
+            depend.analyze();
+            System.out.println("DONE");
+    }
+}
