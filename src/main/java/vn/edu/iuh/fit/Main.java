20b0fd324e977f2fd6139e1dcc55250020d97cc2
H31M44\Student <Student>
2024-03-03 13:58:43 +0700

    add libs

--- src/main/java/vn/edu/iuh/fit/Main.java
+++ src/main/java/vn/edu/iuh/fit/Main.java
@@ -8,8 +8,9 @@ import java.io.PrintWriter;
 public class Main {
     public static void main(String[] args) throws IOException {
             JDepend depend =new JDepend(new PrintWriter("reports/report.xml"));
-            depend.addDirectory("T:\\LeHuuHiep\\KT_Week4");
+            depend.addDirectory("T:\\LeHuuHiep\\Library-Assistant");
             depend.analyze();
+
             System.out.println("DONE");
     }
 }
---
+++ src/main/java/vn/edu/iuh/fit/Swing_UI_Main.java
@@ -0,0 +1,12 @@
+package vn.edu.iuh.fit;
+
+import jdepend.swingui.JDepend;
+
+public class Swing_UI_Main {
+    public static void main(String[] args) throws Exception{
+        JDepend depend =new JDepend();
+        depend.addDirectory("T:\\LeHuuHiep\\KT_Week4\\Library-Assistant");
+        depend.analyze();
+        System.out.println("DONE");
+    }
+}
